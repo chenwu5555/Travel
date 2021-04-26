e17b76989bb85d1059eb55519f927a19c3265ba6
Chen <2234107187@qq.com>
2021-04-26 22:18:52 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 // 引入fastclick 解决移动端click事件300毫秒延时问题
 import fastClick from "fastclick"
+// 引用轮播图插件 VueAwesomeSwiper
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 // 引入清除默认样式的css文件  reset.css
 import "./assets/styles/reset.css"
@@ -10,10 +12,16 @@ import "./assets/styles/reset.css"
 import "./assets/styles/border.css"
 // 引入iconfont-css图标样式
 import "./assets/styles/iconfont.css"
+// 引入轮播图swiper.css的样式
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 
 // 移动端点击延迟处理
 fastClick.attach(document.body)
+
+// 使用轮播图swiper插件
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   router,
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,28 @@
+<template>
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOptions: {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        // Some Swiper option/callback...
+      },
+    };
+  },
+};
+</script>
+
+<style scoped lang="stylus"></style>
